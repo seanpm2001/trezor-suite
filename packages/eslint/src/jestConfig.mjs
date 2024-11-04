12d8ab3790534ecd43920b5d2735d53d5d31ac64
Peter Sanderson <dierk.deidel@protonmail.com>
2024-11-04 12:19:22 +0100

    chore: re-add all our custom eslint rules

--- packages/eslint/src/jestConfig.mjs
+++ packages/eslint/src/jestConfig.mjs
@@ -4,10 +4,15 @@ export const jestConfig = [
     pluginJest.configs['flat/recommended'],
     {
         rules: {
+            // Additions
+            // Enforce arrow functions only is afaik not possible. But this helps.
+            'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
+            'no-console': ['error', { allow: ['warn', 'error'] }],
+
+            // Offs
             'jest/valid-title': 'off', // This rule does not use Typescript and produces false positives
             'jest/valid-describe-callback': 'off', // This rule does not use Typescript and produces false positives
-            'jest/no-disabled-tests': 'off', // Well, what can I say... ¯\_(ツ)_/¯ We skip tests sometimes.
-            'jest/no-focused-tests': 'off', // Same as above, but // Todo: shall be easy to get rid of this
+            'jest/no-disabled-tests': 'off',
             'jest/no-conditional-expect': 'off', // Todo: we shall solve this, this is bad practice
             'jest/expect-expect': 'off', // Todo: we have test with no assertions, this may be legit but it needs to be checked
         },
